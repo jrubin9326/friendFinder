904feedc29e499c49f863172ee0fa1393f0ee392
Jennifer Rubin <jenlrubin9326@gmail.com>
2019-01-09 13:54:39 -0500

    home page created

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 let friendsArray = [{
     "name": "Dwight Schrute",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://media.licdn.com/https://img.purch.com/w/660/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA2NC8zODkvb3JpZ2luYWwvUmFpbm5fV2lsc29uLmpwZw==/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores": [
         5,
         1,
@@ -16,7 +16,7 @@ let friendsArray = [{
 },
 {
     name: "Micheael Scott",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    photo: "https://media.licdn.https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     scores: [
         5,
         1,
@@ -80,7 +80,7 @@ let friendsArray = [{
 },
 {
     "name": "Kevin Malone",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://media.licdn.com/mpr/https://pbs.twimg.com/profile_images/2782329092/a61fe8f72b93fde0d785e77ea4a7107f.jpeg/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores": [
         5,
         1,
@@ -96,7 +96,7 @@ let friendsArray = [{
 },
 {
     "name": "Toby Flenderson",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://https://pbs.twimg.com/media/DYB67_8W4AIXY3d.jpg.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores": [
         5,
         1,
@@ -112,7 +112,7 @@ let friendsArray = [{
 },
 {
     "name": "Kelly Kapoor",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://media.licdn.https://media1.popsugar-assets.com/files/thumbor/eXDFklp6xdWXLyvn9BcH5p-b_rk/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/06/21/951/n/1922283/05e3e789_edit_img_image_41730517_1466542783_kelly-th/i/Kelly-Kapoor-GIFs-From-Office.jpg/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores": [
         5,
         1,
@@ -128,7 +128,7 @@ let friendsArray = [{
 },
 {
     "name": "Angela Martin",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://i.pinimg.com/originals/e5/67/cc/e567cc9331491412a1e53e83c7766a1a.jpg",
     "scores": [
         5,
         1,
@@ -144,7 +144,7 @@ let friendsArray = [{
 },
 {
     "name": "Creed Bratton",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo": "https://media.altpress.com/uploads/2018/07/creed_the_office.jpg",
     "scores": [
         5,
         1,
--- app/htmlRoutes.js
+++ app/htmlRoutes.js
@@ -3,3 +3,12 @@ const path = require('path');
 
 const app = express();
 
+app.get('/:survey', (req, res) => {
+    res.send('ab+cd');
+})
+
+
+
+app.listen(8080, function () {
+    console.log('listening on port 8080....')
+})
