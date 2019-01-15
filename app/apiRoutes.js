7ec699a1d2d6cb798ca72e7e7ae51e218da2d9d7
Jennifer Rubin <jenlrubin9326@gmail.com>
2019-01-14 21:45:18 -0500

    route to friend API

--- app/apiRoutes.js
+++ app/apiRoutes.js
@@ -1,7 +1,7 @@
 // //require data
 const express = require("express");
 let friendsArray = require("./data/friends");
-let router = express.Router();
+
 module.exports = function(app) {
   app.get("/api/friends", (req, res) => {
     res.json(friendsArray);
