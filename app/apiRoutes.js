284c89e391d51ab2c680fe5a10c3a63bc1fd4351
Jennifer Rubin <jenlrubin9326@gmail.com>
2019-01-16 17:19:59 -0500

    changed survey array answers

--- app/apiRoutes.js
+++ app/apiRoutes.js
@@ -1,17 +1,17 @@
 // //require data
-const express = require("express");
 let friendsArray = require("./data/friends");
 
 module.exports = function(app) {
   app.get("/api/friends", (req, res) => {
     res.json(friendsArray);
   });
-};
 
-app.post("/api/friends", (req, res) => {
-  const user = req.body;
-  let userScore = user.scores.reduce((total, answer) => {
-    return parseInt(total) + parseInt(answer);
+  app.post("/api/friends", (req, res) => {
+    const user = req.body;
+    let userScore = user.scores.reduce((total, answer) => {
+      return parseInt(total) + parseInt(answer);
+    });
+
+    console.log(userScore);
   });
-});
-
+};
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,166 +1,63 @@
-let friendsArray = [{
-    "name": "Dwight Schrute",
-    "photo": "https://media.licdn.com/https://img.purch.com/w/660/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA2NC8zODkvb3JpZ2luYWwvUmFpbm5fV2lsc29uLmpwZw==/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-},
-{
+let friendsArray = [
+  {
+    name: "Dwight Schrute",
+    photo:
+      "https://media.licdn.com/https://img.purch.com/w/660/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA2NC8zODkvb3JpZ2luYWwvUmFpbm5fV2lsc29uLmpwZw==/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 1, 1, 5, 1, 1, 5, 1, 1]
+  },
+  {
     name: "Micheael Scott",
-    photo: "https://media.licdn.https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-},
-{
-    "name": "Jim Halpert",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-},
-{
-    "name": "Pam Beesly",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-},
-{
-    "name": "Andy Bernard",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-},
-{
-    "name": "Kevin Malone",
-    "photo": "https://media.licdn.com/mpr/https://pbs.twimg.com/profile_images/2782329092/a61fe8f72b93fde0d785e77ea4a7107f.jpeg/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-},
-{
-    "name": "Toby Flenderson",
-    "photo": "https://https://pbs.twimg.com/media/DYB67_8W4AIXY3d.jpg.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-},
-{
-    "name": "Kelly Kapoor",
-    "photo": "https://media.licdn.https://media1.popsugar-assets.com/files/thumbor/eXDFklp6xdWXLyvn9BcH5p-b_rk/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/06/21/951/n/1922283/05e3e789_edit_img_image_41730517_1466542783_kelly-th/i/Kelly-Kapoor-GIFs-From-Office.jpg/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-},
-{
-    "name": "Angela Martin",
-    "photo": "https://i.pinimg.com/originals/e5/67/cc/e567cc9331491412a1e53e83c7766a1a.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-},
-{
-    "name": "Creed Bratton",
-    "photo": "https://media.altpress.com/uploads/2018/07/creed_the_office.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-}
+    photo:
+      "https://media.licdn.https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 4, 4, 4, 4, 4, 4, 5, 5, 5]
+  },
+  {
+    name: "Jim Halpert",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 1, 2, 3, 4, 5, 4, 3, 2, 1]
+  },
+  {
+    name: "Pam Beesly",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [1, 1, 1, 1, 2, 2, 2, 2, 2, 2]
+  },
+  {
+    name: "Andy Bernard",
+    photo:
+      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 5, 5, 5, 5, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Kevin Malone",
+    photo:
+      "https://media.licdn.com/mpr/https://pbs.twimg.com/profile_images/2782329092/a61fe8f72b93fde0d785e77ea4a7107f.jpeg/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  },
+  {
+    name: "Toby Flenderson",
+    photo:
+      "https://https://pbs.twimg.com/media/DYB67_8W4AIXY3d.jpg.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+  },
+  {
+    name: "Kelly Kapoor",
+    photo:
+      "https://media.licdn.https://media1.popsugar-assets.com/files/thumbor/eXDFklp6xdWXLyvn9BcH5p-b_rk/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/06/21/951/n/1922283/05e3e789_edit_img_image_41730517_1466542783_kelly-th/i/Kelly-Kapoor-GIFs-From-Office.jpg/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+  {
+    name: "Angela Martin",
+    photo:
+      "https://i.pinimg.com/originals/e5/67/cc/e567cc9331491412a1e53e83c7766a1a.jpg",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Creed Bratton",
+    photo: "https://media.altpress.com/uploads/2018/07/creed_the_office.jpg",
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  }
+];
 
-
-
-]
-
-module.exports = friendsArray
+module.exports = friendsArray;
